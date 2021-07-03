b436afc250cb7c784a8ed367ddec5c6754d16809
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-07-03 13:11:56 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	require_once "class.php";
+	$db = new rickmortyDB;
+	$db->connect();
+?>
+<!DOCTYPE html>
+<html>
+    <?php
+        include "components/modules/head.php";
+    ?>
+    <body class="bg-dark text-white">
+        <?php
+            if(isset($_COOKIE['user'])){
+                include "components/modules/navbar.php";
+                include "components/modules/portal.php";
+                echo "<section class='container'>";
+				include "components/modules/search_form.php";
+				include "components/modules/characters.php";
+			    echo "</section>";
+		    }else{
+                include "components/modules/logout.php";
+		    }
+	    ?>
+    </body>
+</html>
