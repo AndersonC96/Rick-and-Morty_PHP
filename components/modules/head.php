2f35cd770414c429ad4458bb36df96ffc4a9e07b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-07-03 14:02:04 -0300

    Create head.php

---
+++ components/modules/head.php
@@ -0,0 +1,14 @@
+<head>
+    <meta name='viewport' content='initial-scale=1.0, user-scalable=no'>
+    <meta charset='utf-8'>
+    <title>Rick and Morty Character Search</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/rater.min.js"></script>
+    <script src="js/jquery.emojiRatings.min.js"></script>
+    <script src="js/js.cookie.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
