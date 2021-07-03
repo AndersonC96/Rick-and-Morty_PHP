f44e04b0839b3b8ebc6fb169889686ffccdcd756
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-07-03 15:25:05 -0300

    favicon

--- components/modules/navbar.php
+++ components/modules/navbar.php
@@ -1,4 +1,5 @@
 <nav id="navbar" class="navbar navbar-dark">
+    <link rel="shortcut icon" href="./favicon.ico"/>
     <a class="navbar-brand osl" href="index.php">Rick and Morty Character Search</a>
     <ul class="nav nav-pills">
         <li class="nav-item">
